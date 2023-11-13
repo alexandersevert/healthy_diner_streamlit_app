995298b19f3cf2327a8b9dbd62469968eff11dcc
Alexander Severt <74572329+alexandersevert@users.noreply.github.com>
2023-11-13 14:51:44 -0800

    Update streamlit_app.py
    Call fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Streamlit App: https://appapp-gfc9hqepsr88pv2ibju2lz.streamlit.app
 
 import streamlit
-import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -13,6 +12,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
+
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,3 +23,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
